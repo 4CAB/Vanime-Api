48856e74e34ddc5e089316b03245863d3513eea9
4CAB <trustmagali@gmail.com>
2022-08-20 08:33:46 -0500

    Refactored Code, added function req()

--- Vanime/api.py
+++ Vanime/api.py
@@ -8,27 +8,36 @@ class Vanime:
         self.session = cloudscraper.CloudScraper()
         self.session.get("https://www3.animeflv.net/")
 
+    def req(self, url, method='GET', data=None):
+        """Hace los requests"""
+        if method == 'GET':
+            try:
+                data = self.session.get(url).text
+            except exceptions.ConnectionError:
+                print('Ha ocurrido un error de conexión.\nReintentandolo')
+                self.req(url, method, data)
+            return data
+        elif method == 'POST':
+            return self.session.post(url, data=data)
+
     def search(self, value: str):
         """Busca animes segun el texto recibido"""
-        data = self.session.post('https://www3.animeflv.net/api/animes/search',
-                                 data={'value': value})
+        data = self.req('https://www3.animeflv.net/api/animes/search',
+                        data={'value': value}, method='POST')
+
         recapcha = api_utils.recapcha(data.text)
         if recapcha:
             return recapcha
 
         new_data = [{'Titulo': i['title'], 'Tipo': i['type'], 'Slug': i['slug'],
-                     'Enlace': f"https://www3.animeflv.net/anime/{i['slug']}"}
-                     for i in data.json()]
+                     'Enlace': f"https://www3.animeflv.net/anime/{i['slug']}"
+                     } for i in data.json()]
 
         return formatters.formatter_list(new_data)
 
     def get_anime(self, slug: str):
         """Obtiene informacion de los animes por medio del slug"""
-        try:
-            data = self.session.get(f'https://www3.animeflv.net/anime/{slug}').text
-        except exceptions.ConnectionError:
-            print('Ha ocurrido un error de conexión.\nReintentandolo')
-            data = self.session.get(f'https://www3.animeflv.net/anime/{slug}').text
+        data = self.req(f'https://www3.animeflv.net/anime/{slug}')
 
         recapcha = api_utils.recapcha(data)
         if recapcha:
@@ -40,11 +49,7 @@ class Vanime:
 
     def get_capitulo(self, slug: str, numero: str, check: bool):
         """Obtiene los enlaces para ver el capitulo"""
-        try:
-            data = self.session.get(f'https://www3.animeflv.net/ver/{slug}-{numero}').text
-        except exceptions.ConnectionError:
-            print('Ha ocurrido un error de conexión.\nReintentandolo')
-            data = self.session.get(f'https://www3.animeflv.net/ver/{slug}-{numero}').text
+        data = self.req(f'https://www3.animeflv.net/ver/{slug}-{numero}')
 
         recapcha = api_utils.recapcha(data)
         if recapcha:
